fa9f97e1975158e36ef94fc2b29d40096a4462a0
xiaohuoni <448627663@qq.com>
2020-08-07 16:13:05 +0800

    feat: deploying-nextjs-app

--- components/learn/Profile.js
+++ components/learn/Profile.js
@@ -2,7 +2,7 @@ import { useGetPoints } from '../../lib/learn/records';
 import { TWITTER_USER_NAME } from '../../lib/constants';
 
 const tweet = points => {
-  const url = `https://twitter.com/intent/tweet?url=https%3A%2F%2Fnextjs-cn.com%2Flearn&text=I%20just%20got%20${points}%20points✨%20on&via=${TWITTER_USER_NAME}`;
+  const url = `https://twitter.com/intent/tweet?url=https%3A%2F%2Fnextjs-cn.com%2Flearn&text=我刚刚在 Next.js 中文官网上获得了${points}%20points✨%20on&via=${TWITTER_USER_NAME}`;
   window.open(url, '_blank');
 };
 
